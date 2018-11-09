ec57f423d069229dc7543c9744d1e5089ae3e0ba
saadmanfarhad <saadmanheavy@gmail.com>
2018-11-09 20:07:45 +0600

    Connected to mongodb and used body-parser

--- server.js
+++ server.js
@@ -5,19 +5,22 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
 var cors = require('cors');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect("127.0.0.1:27017/links");
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -25,8 +28,8 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
