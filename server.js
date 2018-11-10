32859d9df9243d99c89c37bde452dd6c5c8c4ad1
saadmanfarhad <saadmanheavy@gmail.com>
2018-11-10 20:35:40 +0600

    Completed

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+var ShortUrlModel = require('./models/shortUrl');
+
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var validUrl = require('valid-url');
+var shortId = require('shortid');
 
 var app = express();
 
@@ -14,7 +18,7 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
-mongoose.connect("127.0.0.1:27017/shortUrl");
+mongoose.connect("127.0.0.1:27017/shortUrls");
 
 app.use(cors());
 
@@ -36,10 +40,64 @@ app.get("/api/hello", function (req, res) {
 
 app.post("/api/shorturl/new", function (req, res, next) {
   var urlToShorten = req.body.url;
-  console.log(urlToShorten);
-  res.json({
-    url: "urlToShorten"
-  })
+  var urlRegex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+
+  if(urlRegex.test(urlToShorten) === true){
+    shortId.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
+    var shortCode = shortId.generate();
+
+    var data = new ShortUrlModel(
+      {
+        originalUrl: urlToShorten,
+        shortUrl: shortCode
+      }
+    );
+
+    data.save(function (error, short) {
+      if(error){
+        return res.send('Error: ' + error);
+      }
+
+      return res.json({
+        original_url: urlToShorten,
+        short_url: shortCode
+      });
+    })
+  }
+  else {
+    res.json({
+      error: "invalid URL"
+    });
+  }
+});
+
+app.get("/api/shorturl/:shortUrl", function (req, res, next) {
+  var shortUrl = req.params.shortUrl;
+
+  ShortUrlModel.findOne({
+    shortUrl: shortUrl
+  }).exec(function (err, url) {
+    if(!err){
+      if(url !== null){
+        var httpRegex = /^(http|https):\/\//i;
+        var originalUrl = url.originalUrl;
+
+        if(httpRegex.test(originalUrl) === true){
+          res.redirect(originalUrl);
+        }
+        else{
+          res.redirect("http://" + originalUrl);
+        }
+      }
+      else{
+        res.status(404).send('Url not found');
+      }
+    }
+    else{
+      res.send(err);
+    }
+  });
+
 });
 
 
